5e23cdb420b034d4089e3a1e2e0f6c46a0b3127c
nikitalita <69168929+nikitalita@users.noreply.github.com>
2026-02-13 20:13:46 -0800

    remove pk3 and wad provider from web version

--- src/web-extension.ts
+++ src/web-extension.ts
@@ -1,13 +1,9 @@
 
 import * as vscode from 'vscode';
 import { activateDehackedFoldingProvider } from './language/dehackedFoldingProvider';
-import { activate as activatePk3Provider } from './pk3-provider/index';
-import { activate as activateWadProvider } from './wad-provider/index';
 
 export function activate(context: vscode.ExtensionContext) {
     activateDehackedFoldingProvider(context);
-    activatePk3Provider(context);
-    activateWadProvider(context);
 }
 
 export function deactivate() {
